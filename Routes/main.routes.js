ecd9fcd9d73727ad167060d76a0b867ae75cc081
ojavier <ojvilleda6@gmail.com>
2024-08-29 20:30:23 -0600

    modificar app.js

--- Routes/main.routes.js
+++ Routes/main.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const mainController = require('../controllers/main.controller');
 
-router.get('/miInformacion', mainController.getRoot);
+router.get('/reportes', mainController.getRoot);
 
 module.exports = router;
