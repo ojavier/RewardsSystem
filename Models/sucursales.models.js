b7a40ca14d1c801030f6073b17f005731c7ed229
Horizon899 <gaelcasares899@gmail.com>
2024-10-10 19:07:32 -0600

    Arregla query de searchbar sucursales

--- Models/sucursales.models.js
+++ Models/sucursales.models.js
@@ -16,8 +16,9 @@ class Sucursales {
 
     //Metodo para encontrar una sola sucursal con la direccion
     static searchSucursal(id_Usuario, Direccion){
-        const query = "SELECT * FROM Sucursales, SucursalesTieneUsuarios WHERE Sucursales.id_Sucursal = SucursalesTieneUsuarios.id_Sucursal AND id_Usuario = ? AND Direccion = ?";
-        return db.execute(query, [id_Usuario, Direccion]);
+        const direccionparecida = `${Direccion}%`; 
+        const query = "SELECT * FROM Sucursales, SucursalesTieneUsuarios WHERE Sucursales.id_Sucursal = SucursalesTieneUsuarios.id_Sucursal AND id_Usuario = ? AND Direccion LIKE '? ';";
+        return db.execute(query, [id_Usuario, direccionparecida]);
     }
 }
 
